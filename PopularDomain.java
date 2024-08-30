1c143d1cb45e284f67102a2c82bd887f28a81610
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-08-30 10:16:25 +0530

    Add files via upload

---
+++ PopularDomain.java
@@ -0,0 +1,30 @@
+package com.example.travel.Adapters;
+
+public class PopularDomain {
+    public PopularDomain(String s, String s1, String s2, int i, boolean b, double v, String pic1, boolean b1, int i1) {
+    }
+
+    public PopularDomain(String s, String s1) {
+    }
+
+    public int getTitle() {
+        return 0;
+    }
+
+    public int getLocation() {
+        return 0;
+    }
+
+    public int getScore() {
+        return 0;
+    }
+
+    public int getPic() {
+        return 0;
+    }
+
+    public String getPicPath() {
+
+        return "";
+    }
+}
